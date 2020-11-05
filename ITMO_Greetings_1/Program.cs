a2911d00f36e086be349cdc6d1166a9ed257c588
voldanila <73543284+voldanila@users.noreply.github.com>
2020-11-05 17:42:36 +0300

    Добавьте файлы проекта.

---
+++ ITMO_Greetings_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ITMO_Greetings_1
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+
+        }
+    }
+}
